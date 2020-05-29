7cddf365faf4973a4eda7dd3bb8e4d223b8abbdd
Mathew Chanda <mvchanda@ncsu.edu>
2020-05-29 14:34:41 -0400

    Removed Hyperlink

--- src/App.js
+++ src/App.js
@@ -12,14 +12,6 @@ function App() {
         <p>
           I Love Micah White 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
