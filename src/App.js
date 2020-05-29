cc61c6844556075000d32f5c33ad4a58781a7a5f
Mathew Chanda <mvchanda@ncsu.edu>
2020-05-29 14:33:51 -0400

    Added a picture of Micah White

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IMG_0018 from './IMG_0018.jpg'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={IMG_0018} className="App-logo" alt="logo" />
         <p>
-          Hello Everyone! - Ishaan Radia 
+          I Love Micah White 
         </p>
         <a
           className="App-link"
